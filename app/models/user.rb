f84208adde715ea313cf64978b7e9f627c9c1e35
Igor Simdyanov <igorsimdyanov@gmail.com>
2021-12-14 21:52:42 +0300

    Урок 7 Воможности ActiveRecord

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :name, uniqueness: true
   validates :name, length: { maximum: 16, minimum: 2 }
+
+  scope :default, -> { where(role_id: Role.find_by(code: :default).id) }
+  scope :fresh, ->(created_at) { where('created_at > ?', created_at) }
+  scope :default_fresh, ->(created_at) { default.fresh(created_at) }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Item.destroy_all
+Event.destroy_all
+User.destroy_all
+Role.destroy_all
+
+Role.create(name: 'Пользователь', code: :default)
+
+hash_users = 10.times.map do
+  {
+    email: FFaker::Internet.safe_email,
+    name: FFaker::Internet.user_name,
+    role_id: Role.find_by(code: :default).id
+  }
+end
+
+users = User.create hash_users
+
+# User.default_fresh(Time.mktime(2021, 12, 10)).count
+
+hash_events = 20.times.map do
+{
+  name: FFaker::Lorem.phrase,
+  content: FFaker::Lorem.paragraph,
+  user_id: users.sample.id
+}
+end
+
+events = Event.create hash_events
+
+hash_items = 200.times.map do
+  {
+    name: FFaker::HipsterIpsum.paragraph,
+    event_id: events.sample.id
+  }
+end
+Item.create hash_items
