9915779d8cc27f99946dc348fcc3e2170cec65b5
Igor Simdyanov <igorsimdyanov@gmail.com>
2021-12-21 22:08:26 +0300

    Урок I.9

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,14 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :commentable, polymorphic: true
+
+  after_touch :log_comment
+
+  private
+
+  def log_comment
+    Rails.logger.info '#######################################'
+    Rails.logger.info updated_at
+    Rails.logger.info '#######################################'
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,5 @@
 class Item < ApplicationRecord
   belongs_to :event
 
-  validates :title, presence: true
-  validates :title, length: { in: 3..25 }
-  validates :description, length: { maximum: 300 }
+  validates :name, presence: true
 end
