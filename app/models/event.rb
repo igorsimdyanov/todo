c2cbf69dc470c8b6ee710affbb31bfa3613e9c64
Igor Simdyanov <igorsimdyanov@gmail.com>
2021-12-21 18:23:47 +0300

    Домашние задания за 6-7 уроки

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,8 @@ class Event < ApplicationRecord
   has_many :items
   has_many :comments, as: :commentable
   has_many :commentators, through: :comments, source: :user
+
+  validates :title, presence: true
+  validates :title, length: { in: 3..25 }
+  validates :description, length: { maximum: 300 }
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,5 @@
 class Role < ApplicationRecord
   has_many :users
+
+  validates :name, :code, presence: true
 end
