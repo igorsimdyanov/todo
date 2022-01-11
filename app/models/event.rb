5685e3b6b5536da9d0ece936ef328d57aa00a740
Igor Simdyanov <igorsimdyanov@gmail.com>
2022-01-11 13:56:29 +0300

    III.9 Домашнее задание

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,22 @@
 class Event < ApplicationRecord
   belongs_to :user, counter_cache: true
+
   has_many :items, dependent: :destroy
   has_many :comments, as: :commentable
   has_many :commentators, through: :comments, source: :user
 
   validates :name, presence: true
+
+  after_save :count_events
+  after_destroy :recount_events
+
+  private
+
+  def count_events
+    user.increment!(done? ? :events_done_count : :events_not_done_count)
+  end
+
+  def recount_events
+    user.decrement!(done? ? :events_done_count : :events_not_done_count)
+  end
 end
