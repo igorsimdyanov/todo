42bc5e2d4481f9be68ff07a0dc7a644c4256f1f9
Igor Simdyanov <igorsimdyanov@gmail.com>
2022-01-12 13:08:59 +0300

    Fix i18n

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,10 @@ module Todo
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.i18n.available_locales = %i[en ru]
+    config.i18n.default_locale = :ru
+    config.time_zone = 'Moscow'
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
