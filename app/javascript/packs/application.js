dd0a2f13df7f5553d67cdd700fdcdc8016925a29
Igor Simdyanov <igorsimdyanov@gmail.com>
2022-01-31 10:42:56 +0300

    II.5 Домашнее задание

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -22,4 +22,25 @@ window.addEventListener("load", () => {
       element.innerHTML = element.innerHTML == "Active" ? "Disactive" : "Active"
     });
   });
+
+  const email_field = document.querySelector("input#user_email");
+  const submit_button = document.querySelector(".actions input[value^=Sign]");
+  if(email_field) {
+    email_field.addEventListener("input", (event) => {
+      submit_button.disabled = false;
+    });
+  }
+
+  const register_form = document.querySelector("form#new_user");
+  const email_regex = RegExp(/^([\w.*-]+@([\w-]+\.)+[\w-]{2,4})?$/);
+  const email_error = document.querySelector("#email_error");
+  if(register_form) {
+    register_form.addEventListener("submit", (event) => {
+      let match = email_regex.exec(email_field.value);
+      if(!match) {
+        email_error.style.display = 'block';
+        event.preventDefault();
+      }
+    });
+  }
 });
