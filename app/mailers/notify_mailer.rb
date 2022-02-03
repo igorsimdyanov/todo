c1b6778faa682f43382a6cca9268a0e61c003b15
Игорь Симдянов <igorsimdyanov@gmail.com>
2022-02-03 22:04:19 +0300

    Some refactoring (#22)
    * Выносим логические методы пользователя в Rolable-консерн
    
    * Дополнительные правки: убираем костыль со случайным пользователем
    
    * Занятие II.8

---
+++ app/mailers/notify_mailer.rb
@@ -0,0 +1,12 @@
+class NotifyMailer < ApplicationMailer
+  def users_deadlines(user, events)
+    @events = events
+    # attachments['attach.png'] = File.read(Rails.root.join('public/logo.png').to_s)
+
+    mail(
+      to: user.email,
+      bcc: 'admin@example.com',
+      subject: I18n.t('emails.subjects.user_deadlines')
+      )
+  end
+end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,4 +104,13 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    ssl: true,
+    address: 'smtp.yandex.ru',
+    port: 465,
+    authentication: 'login',
+    user_name: 'example@yandex.ru',
+    password: 'pass'
+  }
 end
