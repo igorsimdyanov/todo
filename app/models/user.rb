8586405ff02b1bca5873e28054e037e4c7f86362
Igor Simdyanov <igorsimdyanov@gmail.com>
2022-02-07 20:36:55 +0300

    Аутентификация для Grape-API

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,6 +34,21 @@ class User < ApplicationRecord
 
   act_as_rolable
 
+  before_save :ensure_authentication_token
+
+  def ensure_authentication_token
+    self.authentication_token ||= generate_authentication_token
+  end
+
+  private
+
+  def generate_authentication_token
+    loop do
+      token = Devise.friendly_token
+      break token unless User.where(authentication_token: token).first
+    end
+  end  
+
   def attributes
     { name: name, email: email }
   end
