6194ea218449202413ae5c4abc6a6f8770b60e8b
Igor Simdyanov <igorsimdyanov@gmail.com>
2022-04-04 20:32:42 +0300

    VI.6 Домашнее задание

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,9 +6,9 @@
 #
 #  id          :bigint           not null, primary key
 #  content     :text
-#  done        :boolean          default(FALSE)
 #  finished_at :datetime
 #  name        :string
+#  state       :integer
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #  user_id     :bigint
@@ -22,6 +22,8 @@
 #  fk_rails_...  (user_id => users.id)
 #
 class Event < ApplicationRecord
+  include AASM
+
   belongs_to :user, counter_cache: true
   has_many :items, dependent: :destroy
   has_many :comments, as: :commentable, dependent: :nullify
@@ -29,4 +31,30 @@ class Event < ApplicationRecord
   has_many_attached :files, strict_loading: true
 
   validates :name, presence: true
+
+  enum state: {
+    created: 0,
+    running: 10,
+    pending: 20,
+    finished: 30
+  }
+
+  aasm column: 'state' do
+    state :created, initial: true, display: I18n.t('state.created')
+    state :running, display: I18n.t('state.running')
+    state :pending, display: I18n.t('state.pending')
+    state :finished, display: I18n.t('state.finished')
+
+    event :start do
+      transitions from: %i[created running pending], to: :running
+    end
+
+    event :pend do
+      transitions from: %i[running pending created], to: :pending
+    end
+
+    event :complete do
+      transitions from: %i[running finished], to: :finished
+    end
+  end
 end
--- lib/tasks/aasm_diagram.rake
+++ lib/tasks/aasm_diagram.rake
@@ -4,4 +4,10 @@ namespace :aasm_diagram do
     AASMDiagram::Diagram.new(TrafficLight.new.aasm, 'tmp/traffic_light.png')
     puts 'Диаграмма создана по пути tmp/traffic_light.png'
   end
+
+  desc 'Генерация png-диаграммы для модели Event'
+  task event: :environment do
+    AASMDiagram::Diagram.new(Event.new.aasm, 'tmp/event.png')
+    puts 'Диаграмма создана по пути tmp/event.png'
+  end
 end
