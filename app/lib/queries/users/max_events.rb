44bec69661954b15f685e06be4cc51b83383a3e1
Igor Simdyanov <igorsimdyanov@gmail.com>
2022-08-20 22:28:41 +0300

    Try to user short form of module

--- app/lib/queries/users/max_events.rb
+++ app/lib/queries/users/max_events.rb
@@ -1,25 +1,20 @@
 # frozen_string_literal: true
 
-module Queries
-  module Users
-    class MaxEvents
-      include Callable
-      extend Dry::Initializer
+module Queries::Users::MaxEvents
+  include Callable
+  extend Dry::Initializer
 
-      param :max_count, default: proc { 3 }
+  param :max_count, default: proc { 3 }
 
-      def call
-        User.where(id: user_ids)
-      end
-
-      private
-
-      def user_ids
-        Event.group(:user_id)
-             .count
-             .max_by(max_count) { |_user_id, count| count }
-             .map(&:first)
-      end
-    end
+  def call
+    User.where(id: user_ids)
   end
+
+  private
+
+  def user_ids
+    Event.group(:user_id)
+         .count
+         .max_by(max_count) { |_user_id, count| count }
+         .map(&:first)
 end
